7cd5ffe7f0c92931daa37f7b61c5ee81158f44ef
Jakub Trzebiatowski <cubuspl42@gmail.com>
2016-10-19 23:07:42 +0200

    Check if va_* macros are defined before defining them

--- inc/efistdarg.h
+++ inc/efistdarg.h
@@ -21,11 +21,18 @@ Revision History
 
 #ifndef GNU_EFI_USE_EXTERNAL_STDARG
 typedef __builtin_va_list va_list;
-
-# define va_start(v,l)	__builtin_va_start(v,l)
-# define va_end(v)	__builtin_va_end(v)
-# define va_arg(v,l)	__builtin_va_arg(v,l)
-# define va_copy(d,s)	__builtin_va_copy(d,s)
+# ifndef va_start
+#  define va_start(v,l)	__builtin_va_start(v,l)
+# endif
+# ifndef va_end
+#  define va_end(v)	__builtin_va_end(v)
+# endif
+# ifndef va_arg
+#  define va_arg(v,l)	__builtin_va_arg(v,l)
+# endif
+# ifndef va_copy
+#  define va_copy(d,s)	__builtin_va_copy(d,s)
+# endif
 #else
 # include <stdarg.h>
 #endif
